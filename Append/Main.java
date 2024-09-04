e8b85d497e0e591e261fcd4f9bcb9abc999b6251
Abhishek Gupta <abhishek8276gupta@gmail.com>
2024-09-05 00:42:42 +0530

    Append file successfully added

---
+++ Append/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Append myLinkedList = new Append (5);
+        myLinkedList.printAll();
+        
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 4
+            Tail: 4
+            Length: 1
+            
+            Linked List:
+            4
+
+        */
+
+    }
+
+}
